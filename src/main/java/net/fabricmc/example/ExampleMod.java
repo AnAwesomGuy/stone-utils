6fd826a5fbe09d18b8369a33bc8dadd1603b7a19
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2023-04-15 11:38:27 +0800

    Update ExampleMod.java

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,21 +1,18 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.util.Identifier;
+import net.minecraft.item.ShearsItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 
 public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+ 
+    // an instance of our new item
+    public static final Item SHEARS = new ShearsItem(new Item.Settings().maxDamage(238).group(ItemGroup.TOOLS));
+ 
+    @Override
+    public void onInitialize() {
+        Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_item"), CUSTOM_ITEM);
+    }
 }
