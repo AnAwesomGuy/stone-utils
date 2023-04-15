5592c57edba61d3e49a1669b55936fe2296f6090
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2023-04-15 16:10:07 +0800

    Update and rename ExampleMod.java to StoneShears.java

--- src/main/java/net/fabricmc/example/StoneShears.java
+++ src/main/java/net/fabricmc/example/StoneShears.java
@@ -1,21 +1,18 @@
 package net.fabricmc.example;
 
+import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.ShearsItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class ExampleMod {
+public class StoneShears extends ModInitializer {
  
     public static final Item STONE_SHEARS = new ShearsItem(new Item.Settings().maxDamage(87).group(ItemGroup.TOOLS));
  
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
- 
-    public static void registerItems() {
-        Registry.register(Registry.ITEM, new Identifier("kubejs", "stone_shears"), STONE_SHEARS);
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier("stone_shears", "fabric_item"), FABRIC_ITEM);
     }
 }
