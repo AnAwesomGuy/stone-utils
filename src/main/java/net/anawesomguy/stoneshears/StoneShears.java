11fbb3c0d01bb7707610e0101346b6b92a0aa6fc
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2023-04-15 17:03:25 +0800

    Update StoneShears.java

--- src/main/java/net/anawesomguy/stoneshears/StoneShears.java
+++ src/main/java/net/anawesomguy/stoneshears/StoneShears.java
@@ -7,12 +7,12 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class StoneShears extends ModInitializer {
+public class StoneShears implements ModInitializer {
  
-    public static final Item STONE_SHEARS = new ShearsItem(new Item.Settings().maxDamage(87).group(ItemGroup.TOOLS));
+    public static final Item STONE_SHEARS = new ShearsItem(new Item.Settings().maxDamage(93).group(ItemGroup.TOOLS));
  
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("stone_shears", "fabric_item"), FABRIC_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("stone_shears", "stone_shears"), STONE_SHEARS);
     }
 }
