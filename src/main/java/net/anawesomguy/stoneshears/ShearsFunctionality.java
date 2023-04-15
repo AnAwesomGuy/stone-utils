79bb743d946e167c24246ddade7514e46743730a
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2023-04-16 07:52:11 +0800

    Create ShearsFunctionality.java

---
+++ src/main/java/net/anawesomguy/stoneshears/ShearsFunctionality.java
@@ -0,0 +1,19 @@
+package net.anawesomguy.stoneshears
+
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.anawesomguy.stoneshears.StoneShears;
+import net.minecraft.loot.entry.ItemEntry;
+
+public class ShearsFunctionality {
+  static {
+    LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+      if (source.isBuiltin() && COAL_ORE_LOOT_TABLE_ID.equals(id)) {
+          LootPool.Builder poolBuilder = LootPool.builder()
+            .with(ItemEntry.builder(Items.EGG));
+          tableBuilder.pool(poolBuilder);
+      }
+    });
+  }
+}
