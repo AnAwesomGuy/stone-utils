a4635ce69a48fc46c94c488174e1852f97dea48f
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2023-04-16 09:08:16 +0800

    Update StoneShears.java

--- src/main/java/net/anawesomguy/stoneshears/StoneShears.java
+++ src/main/java/net/anawesomguy/stoneshears/StoneShears.java
@@ -2,6 +2,10 @@ package net.anawesomguy.stoneshears;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.ShearsItem;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -15,10 +19,16 @@ public class StoneShears implements ModInitializer {
  
   @Override
   public void onInitialize() {
-      Registry.register(Registry.ITEM, new Identifier("stone_shears", "stone_shears"), STONE_SHEARS);
-  }
-    
-  public static void addDispenserBehaviours() {
-      DispenserBlock.registerBehavior(STONE_SHEARS, new ShearsDispenserBehavior());
+    Registry.register(Registry.ITEM, new Identifier("stone_shears", "stone_shears"), STONE_SHEARS);
+
+    DispenserBlock.registerBehavior(STONE_SHEARS, new ShearsDispenserBehavior());
+
+    LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+      if (source.isBuiltin() && COAL_ORE_LOOT_TABLE_ID.equals(id)) {
+        LootPool.Builder poolBuilder = LootPool.builder()
+          .with(ItemEntry.builder(Items.EGG));
+        tableBuilder.pool(poolBuilder);
+      }
+    });
   }
 }
