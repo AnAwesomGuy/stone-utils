5af053d5da51f7dd7c6adf8790255e968c887df3
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2023-04-16 09:59:02 +0800

    Update StoneShears.java

--- src/main/java/net/anawesomguy/stoneshears/StoneShears.java
+++ src/main/java/net/anawesomguy/stoneshears/StoneShears.java
@@ -25,15 +25,13 @@ public class StoneShears implements ModInitializer {
     Registry.register(Registry.ITEM, new Identifier("stone_shears", "stone_shears"), STONE_SHEARS);
 
     DispenserBlock.registerBehavior(STONE_SHEARS, new ShearsDispenserBehavior());
-		
-		static {
-			LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-				if (source.isBuiltin() && COAL_ORE_LOOT_TABLE_ID.equals(id)) {
-					LootPool.Builder poolBuilder = LootPool.builder()
-						.with(ItemEntry.builder(STONE_SHEARS));
-					tableBuilder.pool(poolBuilder);
-				}
-			});
-		}
+
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if (source.isBuiltin() && COAL_ORE_LOOT_TABLE_ID.equals(id)) {
+				LootPool.Builder poolBuilder = LootPool.builder()
+					.with(ItemEntry.builder(STONE_SHEARS));
+				tableBuilder.pool(poolBuilder);
+			}
+		});
   }
 }
