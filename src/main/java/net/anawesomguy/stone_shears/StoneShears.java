ebea74a1199119bb03746363ce056b55edf87ac8
AnAwesomGuy <evanyweng@gmail.com>
2023-07-15 08:59:34 +0800

    did a dumb

--- src/main/java/net/anawesomguy/stone_shears/StoneShears.java
+++ src/main/java/net/anawesomguy/stone_shears/StoneShears.java
@@ -1,9 +1,5 @@
 package net.anawesomguy.stone_shears;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonNull;
-import net.anawesomguy.stone_shears.mixin.JsonArrayInvoker;
-import net.anawesomguy.stone_shears.mixin.JsonObjectInvoker;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.dispenser.ShearsDispenserBehavior;
@@ -21,12 +17,5 @@ public class StoneShears implements ModInitializer {
   public void onInitialize() {
     Registry.register(Registry.ITEM, STONE_SHEARS_ITEM_ID, STONE_SHEARS);
     DispenserBlock.registerBehavior(STONE_SHEARS, new ShearsDispenserBehavior());
-  }
-  @SuppressWarnings("unchecked")
-  public static <T extends JsonElement> T deepCopy(T json) {
-    if (json.isJsonNull()) return (T)JsonNull.INSTANCE;
-    if (json.isJsonArray()) return (T)((JsonArrayInvoker)json).invokeDeepCopy();
-    if (json.isJsonObject()) return (T)((JsonObjectInvoker)json).invokeDeepCopy();
-    return json;
   }
 }
--- src/main/java/net/anawesomguy/stone_shears/mixin/MatchToolLootConditionMixin.java
+++ src/main/java/net/anawesomguy/stone_shears/mixin/MatchToolLootConditionMixin.java
@@ -10,18 +10,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import static net.anawesomguy.stone_shears.StoneShears.STONE_SHEARS_ITEM_ID;
-import static net.anawesomguy.stone_shears.StoneShears.deepCopy;
 
 @Mixin(MatchToolLootCondition.class)
 public abstract class MatchToolLootConditionMixin {
     @ModifyVariable(at = @At("HEAD"), method = "<init>", argsOnly = true)
     private static ItemPredicate shearsLoot(ItemPredicate predicate) {
-        JsonElement element = deepCopy(predicate.toJson());
+        JsonElement element = predicate.toJson();
         JsonObject object = element.isJsonObject() ? element.getAsJsonObject() : null;
         if (object != null) {
             JsonArray items = object.getAsJsonArray("items");
             if (items != null) {
-                for (JsonElement jsonElement : deepCopy(items)) {
+                for (JsonElement jsonElement : ((JsonArrayInvoker)(JsonElement)items).invokeDeepCopy()) {
                     if (jsonElement.getAsString().equals("minecraft:shears")) {
                         items.add(STONE_SHEARS_ITEM_ID.toString());
                     }
